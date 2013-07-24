80766ca55a286ba6d4d0f27a20ab0ac6b0b63e9c
Cristiano Belloni <janesconference@bitterspring.net>
2013-07-24 11:23:19 +0100

    Changed define signature to work with the jspm loader.js

--- ksp.js
+++ ksp.js
@@ -1,4 +1,4 @@
-define(['require', 'kievII', 'image'], function(require, K2) {
+define(['require', 'github:janesconference/KievII@0.5/dist/kievII'], function(require, K2) {
 
     var imgResources = null;
 
